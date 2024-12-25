cb317314637df7dd9f3f03b95033483def0f21ef
Jual Rana <contactjual@gmail.com>
2024-12-25 11:42:09 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+
+// variable declare 
+
+$name = true;
+$first_name = "Jual";
+$last_name = "Rana";
+$birth_day = "22 November";
+$professionalty = 'developer';
+$have_laptop = true;
+$price = 100000;
+$laptop_brand = 'hp';
+
+
+echo 'Hi, I am '.$first_name.' '.$last_name.'. My birthday is '.$birth_day.'. I am a professional '.$professionalty.'. My laptop brand is '.$laptop_brand.' and price '.$price.' TK. ';
+
+// or
+
+echo "Hi, I am ".$first_name." ".$last_name.". My birthday is ".$birth_day.". I am a professional ".$professionalty.". My laptop brand is ".$laptop_brand." and price ".$price."TK. ";
+
+// or 
+
+echo "Hi, I am $first_name $last_name. My birthday is $birth_day. I am a professsional $professionalty. My laptop brand is $laptop_brand and price $price TK. "; // best practice
+                                        
+
+
+
+
+// if use $$ sign
+
+$my_first_name = "Jual";
+$naming = "my_first_name";
+
+echo $$naming; // $$naming = $my_first_name = Jual
+
+
+
+?>
